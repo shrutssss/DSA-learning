6ec5c7c2a9386a10c6c4cfd4d7af20116380ac9a
shrutssss <shrutiv.jahagirdar@gmail.com>
2025-07-29 18:30:18 +0530

    update comments

--- basics/nthFibbonacci.cpp
+++ basics/nthFibbonacci.cpp
@@ -1,28 +1,29 @@
 // Nth Fibbonacci number: 
 // Problem statement: 
-// The n-th term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula 
-// F(n) = F(n - 1) + F(n - 2), 
-//     Where, F(1) = 1, F(2) = 1
-// Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases like when 'n' = 1 or 'n' = 2 by using conditionals like if else and return what's expected.
-// problem link: https:www.naukri.com/code360/problems/nth-fibonacci-number_74156
+// The n-th term of Fibonacci series, *consider that indexing starts from 1.
+// problem link: https://www.naukri.com/code360/problems/nth-fibonacci-number_74156
 
-// my solution:
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int n;
     cin>>n;
+    //declare first, second numbers of the series as 1. 
+    //initialise a third variable as 0 for further use.
     int first=1,second=1,third=0;
+    // for loop: starts from 3rd term (i=2 because we already have first two terms)
     for(int i=2;i<=n-1;i++){
         third=first+second;
         first=second;
         second=third;
     }
+    //handling edge cases if input is 1 or 2
+    //else printing the nth number from the series (stored in third variable).
     if(n==1||n==2){
         cout<<"1";
     }else{
-        cout<<third;
+        cout<<"The"<<n<<"th Fibbonacci number is: "<<third;
     }
     return 0;
 }
