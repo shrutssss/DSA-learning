79e3785a8b8efd3282bff2ffd3f196afaf8b5744
shrutssss <shrutiv.jahagirdar@gmail.com>
2025-07-29 18:29:04 +0530

    update comments

--- basics/nthFibbonacci.cpp
+++ basics/nthFibbonacci.cpp
@@ -4,10 +4,6 @@
 // F(n) = F(n - 1) + F(n - 2), 
 //     Where, F(1) = 1, F(2) = 1
 // Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases like when 'n' = 1 or 'n' = 2 by using conditionals like if else and return what's expected.
-// "Indexing is start from 1"
-// Example :
-// Input: 6
-// Output: 8
 // problem link: https:www.naukri.com/code360/problems/nth-fibonacci-number_74156
 
 // my solution:
