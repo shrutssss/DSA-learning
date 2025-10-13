628ee14d5fa7010df911f0726c4ee524b245d8f5
shrutssss <shrutiv.jahagirdar@gmail.com>
2025-10-13 21:14:36 +0530

    initial commit

---
+++ patterns/11.BinaryNumberTrianglePattern.cpp
@@ -0,0 +1,29 @@
+// Binary Number Triangle Pattern
+// problem link: 
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j) % 2 == 0){
+                    cout<<1<<" ";
+                }else{
+                    cout<<0<<" ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n;
+    cout<<"Enter n: "<<endl;
+    cin>>n;
+    Solution obj;
+    obj.printTriangle(n)
+}
